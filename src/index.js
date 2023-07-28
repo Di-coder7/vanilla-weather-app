c372d8b1cb7fd1a733fd0968905216b523b2d2f1
Di-coder7 <doroshenko.dasha97@gmail.com>
2023-07-28 19:57:23 +0200

    styled all content and added coder string

---
+++ src/index.js
@@ -0,0 +1,78 @@
+    function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
+    return `${day}, ${hours}:${minutes}`;
+  }
+  
+  function displayWeatherCondition(response) {
+    document.querySelector("#actual-city").innerHTML = response.data.city;
+    document.querySelector("#actual-date-time").innerHTML = formatDate(
+      response.data.time * 1000
+    );
+    document.querySelector("#temperature").innerHTML = Math.round(
+      response.data.temperature.current
+    );
+    document.querySelector("#humidity").innerHTML =
+      response.data.temperature.humidity;
+    document.querySelector("#wind").innerHTML = Math.round(
+      response.data.wind.speed
+    );
+    document.querySelector("#description").innerHTML =
+      response.data.weather[0].main;
+    document.querySelector("#h1-icon").setAttribute(
+        "src",
+        `https://ssl.gstatic.com/onebox/weather/64/${response.data.condition.icon}.png`
+      );
+    document
+      .querySelector("#h1-icon")
+      .setAttribute("alt", response.data.condition.description);
+  }
+  
+  function searchCity(city) {
+    let apiKey = "d527df6aa83c9f534ae4562144311714";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeatherCondition);
+  }
+  
+  function handleSubmit(event) {
+    event.preventDefault();
+    let city = document.querySelector("#enter-city").value;
+    searchCity(city);
+  }
+  
+  function searchLocation(position) {
+    let apiKey = "d527df6aa83c9f534ae4562144311714";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${position.coords.longitude}&lat=${position.coords.latitude}&key=${apiKey}&units=metric`;
+  
+    axios.get(apiUrl).then(displayWeatherCondition);
+  }
+  
+  function getCurrentLocation(event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(searchLocation);
+  }
+  
+  let searchForm = document.querySelector("#search-city");
+  searchForm.addEventListener("submit", handleSubmit);
+  
+  let currentLocationButton = document.querySelector("#actual-location");
+  currentLocationButton.addEventListener("click", getCurrentLocation);
+  
+  searchCity("Berlin");
