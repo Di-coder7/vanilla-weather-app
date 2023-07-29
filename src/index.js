26bb1a7cf28798af57f12d4bec04f81a62a3e249
Di-coder7 <doroshenko.dasha97@gmail.com>
2023-07-29 17:19:00 +0200

    added current time, relevant icon and description of temperature

--- src/index.js
+++ src/index.js
@@ -1,78 +1,66 @@
-    function formatDate(timestamp) {
-    let date = new Date(timestamp);
-    let hours = date.getHours();
-    if (hours < 10) {
-      hours = `0${hours}`;
-    }
-    let minutes = date.getMinutes();
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-    let day = days[date.getDay()];
-    return `${day}, ${hours}:${minutes}`;
+// show current time
+
+let date = new Date();
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Thuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+
+  let hour = [date.getHours()];
+  if (hour < 10) {
+    hour = `0 ${hour}`;
   }
-  
-  function displayWeatherCondition(response) {
-    document.querySelector("#actual-city").innerHTML = response.data.city;
-    document.querySelector("#actual-date-time").innerHTML = formatDate(
-      response.data.time * 1000
-    );
-    document.querySelector("#temperature").innerHTML = Math.round(
-      response.data.temperature.current
-    );
-    document.querySelector("#humidity").innerHTML =
-      response.data.temperature.humidity;
-    document.querySelector("#wind").innerHTML = Math.round(
-      response.data.wind.speed
-    );
-    document.querySelector("#description").innerHTML =
-      response.data.weather[0].main;
-    document.querySelector("#h1-icon").setAttribute(
-        "src",
-        `https://ssl.gstatic.com/onebox/weather/64/${response.data.condition.icon}.png`
-      );
-    document
-      .querySelector("#h1-icon")
-      .setAttribute("alt", response.data.condition.description);
+  let minutes = [date.getMinutes(2)];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
   }
-  
-  function searchCity(city) {
-    let apiKey = "d527df6aa83c9f534ae4562144311714";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayWeatherCondition);
-  }
-  
-  function handleSubmit(event) {
-    event.preventDefault();
-    let city = document.querySelector("#enter-city").value;
-    searchCity(city);
-  }
-  
-  function searchLocation(position) {
-    let apiKey = "d527df6aa83c9f534ae4562144311714";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${position.coords.longitude}&lat=${position.coords.latitude}&key=${apiKey}&units=metric`;
-  
-    axios.get(apiUrl).then(displayWeatherCondition);
-  }
-  
-  function getCurrentLocation(event) {
-    event.preventDefault();
-    navigator.geolocation.getCurrentPosition(searchLocation);
-  }
-  
-  let searchForm = document.querySelector("#search-city");
-  searchForm.addEventListener("submit", handleSubmit);
-  
-  let currentLocationButton = document.querySelector("#actual-location");
-  currentLocationButton.addEventListener("click", getCurrentLocation);
-  
-  searchCity("Berlin");
+
+  let currentDay = days[date.getDay()];
+
+  return `${currentDay} ${hour}:${minutes}`;
+}
+let now = document.querySelector("#actual-date-time");
+now.innerHTML = formatDate(date);
+
+// show real weather and city
+
+function displayWeather(response) {
+
+    console.log(response.data.temperature.current);
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+    let cityElement = document.querySelector("#actual-city");
+    cityElement.innerHTML = response.data.city;
+    let descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML = response.data.condition.description;
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = response.data.temperature.humidity;
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    let iconElement = document.querySelector("#h1-icon");
+    iconElement.setAttribute("src", `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`);
+    iconElement.setAttribute("alt", response.data.condition.description)
+
+}
+
+
+
+
+let apiKey = "dcdf4529bd3tb420ca3d3f41eaod213b";
+let query = "New York";
+let units = "metric";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${query}&key=${apiKey}&units=${units}`;
+console.log(apiUrl); 
+
+axios.get(apiUrl).then(displayWeather);
+
+
+
+
+
